d30faa733abd5fba907b468b214b32c5cbb35ca3
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-16 19:12:22 -0300

    doctests

--- calculator.py
+++ calculator.py
@@ -1,6 +1,33 @@
 
 
 def sums(x, y):
+    """sum
+
+    Args:
+        x (float): Number
+        y (float): Number
+
+    Returns:
+        _type_: Number
+
+
+    >>> sums(10, 20)
+    30
+
+    >>> sums(-10, 20)
+    10
+
+    >>> sums('10', 20)
+    Traceback (most recent call last):
+    ...
+    AssertionError: 10 needs to be int or float
+
+    """
     assert isinstance(x, (int, float)), f'{x} needs to be int or float'
     assert isinstance(y, (int, float)), f'{y} needs to be int or float'
     return x + y
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
